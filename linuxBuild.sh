b0eebad5f8209feec1648b3088f56506ef65963b
mikeyd <mdeguzis@gmail.com>
2016-02-25 19:26:46 -0500

    Update linuxBuild.sh

--- linuxBuild.sh
+++ linuxBuild.sh
@@ -1,5 +1,42 @@
 #!/bin/bash
 
+
+##########################################
+# INFO
+##########################################
+: <<'END'
+
+# Known flags
+# Updated 20160225 - ProfessorKaos64
+BUILD_GAMEPAK='1'
+JOBS='1'
+NO_GCH='0'
+DEDICATED='0'
+SILENT='0'
+GL_HARDLINK='0'
+ID_MCHECK='2'
+BUILD_ROOT='build'
+ALSA='1'
+CC='gcc'
+TARGET_CORE='1'
+BUILD='release'
+TARGET_GAME='1'
+TARGET_MONO='0'
+BASEFLAGS=''
+CXX='g++'
+NOCURL='0'
+DEBUG_MEMORY='0'
+IDNET_HOST=''
+LIBC_MALLOC='1'
+ID_NOLANADDRESS='0'
+TARGET_DEMO='0'
+Command line: BUILD_GAMEPAK='1'
+Command line: NO_GCH='0'
+Command line: BUILD='release'
+END
+
+##########################################
+
 mkdir -p ~/games/tdm/darkmod
 
 # make sure this file exists
